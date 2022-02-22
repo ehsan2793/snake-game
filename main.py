021c74b34d8104abb9b58a6801fb18fdf0a56503
ehsan2793 <ehsan2793@gmail.com>
2022-02-22 15:50:04 -0800

    snake moves forward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import time
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 
 screen = Screen()
 screen.colormode(255)
@@ -10,41 +11,17 @@ screen.title('Snake Game')
 screen.tracer(0)
 screen.listen()
 
-colors = ['red', 'green', 'blue']
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-snake_parts = []
-
-def move_forward():
-    for i in snake_parts:
-        i.forward(20)
-
-# todo: erase the unwanted colors
-# snake creation
-i = 0
-for position in starting_position:
-    segment = Turtle('square')
-
-    segment.color(colors[i])
-    i+= 1
-    segment.penup()
-    segment.setposition(position)
-    snake_parts.append(segment)
-
-
+snake = Snake()
+screen.onkey(snake.up,'Up')
+screen.onkey(snake.down,'Down')
+screen.onkey(snake.left,'Left')
+screen.onkey(snake.right,'Right')
+screen.onkey(snake.change_color,'c')
 game_is_on = True
 # snake move
 while game_is_on:
     screen.update()
-    for index in range( len(snake_parts)-1,0,-1):
-        position=snake_parts[index - 1].position()
-        snake_parts[index].setposition(position)
-        time.sleep(0.2)
-    snake_parts[0].forward(20)
-
-
-
-
-
-
+    time.sleep(0.1)
+    snake.move()
 
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,49 @@
+import random
+from turtle import Turtle
+import time
+
+STRING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+class Snake:
+    def __init__(self):
+        self.snake_parts = []
+        self.create_snake()
+        self.head = self.snake_parts[0]
+        self.head.color('orange')
+        self.head.shape('circle')
+
+    def create_snake(self):
+        for position in STRING_POSITION:
+            segment = Turtle('square')
+            segment.color('white')
+            segment.penup()
+            segment.setposition(position)
+            self.snake_parts.append(segment)
+
+    def move(self):
+        for index in range(len(self.snake_parts) - 1, 0, -1):
+            position = self.snake_parts[index - 1].position()
+            self.snake_parts[index].setposition(position)
+        self.head.forward(DISTANCE)
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+    def change_color(self):
+        r =random.randint(0,255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        self.head.color((r,g,b))
+
